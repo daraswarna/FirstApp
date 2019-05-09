851a46cda69f89e6897646602181359a6c431f88
Lenovo <Lenovo@DESKTOP-C7PCAIT>
2019-05-09 15:21:57 +0530

    TASK-1234

---
+++ src/com/app/Module.java
@@ -0,0 +1,23 @@
+package com.app;
+
+public class Module {
+
+	private int modId;
+	private String modName;
+	public Module() {
+		super();
+	}
+	public int getModId() {
+		return modId;
+	}
+	public void setModId(int modId) {
+		this.modId = modId;
+	}
+	public String getModName() {
+		return modName;
+	}
+	public void setModName(String modName) {
+		this.modName = modName;
+	}
+	
+}
---
+++ src/com/app/Project.java
@@ -0,0 +1,25 @@
+package com.app;
+
+public class Project {
+
+	private int prjId;
+	private String prjName;
+	public Project() {
+		super();
+	}
+	public int getPrjId() {
+		return prjId;
+	}
+	public void setPrjId(int prjId) {
+		this.prjId = prjId;
+	}
+	public String getPrjName() {
+		return prjName;
+	}
+	public void setPrjName(String prjName) {
+		this.prjName = prjName;
+	}
+	
+	
+	
+}
